9e4cb9fb08a22d2f1d38c823b6391391309ad597
Vince <vince.maano1@gmail.com>
2025-03-29 03:18:18 +0800

    added all of the functions
    also really short sooo

--- snake_case_converter.py
+++ snake_case_converter.py
@@ -1,5 +1,9 @@
 #Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 
 #the actual program
+user_name = input("What is your full name? (input it in incorrect casing please) = ")
+snake_case_name = '_'.join(word.lower() for word in user_name.split())
 
 #print
+print(f"Here is your name in snake_case = {snake_case_name}")
+
