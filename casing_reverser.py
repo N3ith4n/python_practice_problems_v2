458104fba130994950186b1182009f5931124731
Vince <vince.maano1@gmail.com>
2025-03-29 02:46:34 +0800

    added all of the functions
    I also changed the empty list into an empty string as when it was empty list each character is printed separately. I added them all at once was again as it is pretty short

--- casing_reverser.py
+++ casing_reverser.py
@@ -1,9 +1,14 @@
 #Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 
 #asks the user for their full name
+user_name = input("What is your full name? = ")
 
-#empty list where the changed characters will be stored
+#empty string where the changed characters will be stored
+reversed = " "
 
 #loop that will check each character and manually change their casing
+for a in user_name:
+    reversed += a.swapcase()
 
 #print the empty list
+print(reversed)
