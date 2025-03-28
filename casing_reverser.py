d121c199afa4041331925d7a99f836492ad68e84
Vince <vince.maano1@gmail.com>
2025-03-29 02:34:32 +0800

    Create casing_reverser.py and added the pseudocode

---
+++ casing_reverser.py
@@ -0,0 +1,9 @@
+#Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+#asks the user for their full name
+
+#empty list where the changed characters will be stored
+
+#loop that will check each character and manually change their casing
+
+#print the empty list
