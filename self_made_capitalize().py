72737fdbc1e8fe13636642ed198e911c77989c9b
Vince <vince.maano1@gmail.com>
2025-03-29 00:57:13 +0800

    added the custom_capitalize and the input function

--- self_made_capitalize().py
+++ self_made_capitalize().py
@@ -1,5 +1,11 @@
 #make a function that functions just like capitalize()
+def custom_capitalize(input_string):
+    if len(input_string) == 0:
+        return input_string
+      
+    return input_string[0].upper() + input_string[1:].lower()
 
 #asks for user input
+user_input = input("Enter a random word = ")
 
 #print
