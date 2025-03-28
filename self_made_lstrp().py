95236345b47691d55d69c0dfdabd8f13141efcc8
Vince <vince.maano1@gmail.com>
2025-03-28 22:44:19 +0800

    renamed self_made_lstrp() to self_made_lstrp().py and added the def and input function
    I forgot to input that I added the pseudocode on the previous commit

---
+++ self_made_lstrp().py
@@ -0,0 +1,15 @@
+#make a function that will find the first charactr in the inputted string
+def custom_lstrip(s):
+    start_index = 0
+    
+    # Finds the first non-space character
+    while start_index < len(s) and s[start_index] == " ":
+        start_index += 1
+
+    #this returns for example s[2:] so the print will start at the letter that is in the start_index
+    return s[start_index:]
+
+#asks the user to input their name + some spaces before it
+name = input("Please enter your full name and please add some space at front = ")
+
+#print
