a81d41c66b19c5525acbd90e5f05f7c64f387acc
Vince <vince.maano1@gmail.com>
2025-03-29 03:12:19 +0800

    added all of the functions
    pretty short so I added it all at once

--- pascal_case_converter.py
+++ pascal_case_converter.py
@@ -1,5 +1,8 @@
 #Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
 #the actual program
+user_name = input("What is your full name? = ").title().split()
+pascal_case_name = ''.join(word.title() for word in user_name)
 
 #print
+print(f"Here is your name in Pascal Case = {pascal_case_name}")
