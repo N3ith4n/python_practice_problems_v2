64d53488176eb477d80c9eccb6a90f54dd7983aa
Vince <vince.maano1@gmail.com>
2025-03-29 00:45:42 +0800

    added the custom_center and input function

--- self_made_center().py
+++ self_made_center().py
@@ -1,5 +1,16 @@
 #make a function that functions like center()
+def custom_center(input_string, width):
+    if len(input_string) >= width:
+        return input_string
+    
+    total_spaces = width - len(input_string)
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+
+    return ' ' * left_spaces + input_string + ' ' * right_spaces
 
 #enter user input
+user_input = input("Enter a random word = ")
+width = int(input("Enter your desired width for the string = "))
 
 #print
