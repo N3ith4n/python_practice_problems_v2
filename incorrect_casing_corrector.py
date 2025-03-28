88a754892834f2ec6370c56c9efe775b069ef8fe
Vince <vince.maano1@gmail.com>
2025-03-29 02:32:02 +0800

    changed the pseudocode and added all the functions
    realized that you can just use .title() for this so I changed my plans and made it this way

--- incorrect_casing_corrector.py
+++ incorrect_casing_corrector.py
@@ -1,7 +1,7 @@
 #Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 
-#asks for user input
-
 #the actual program
+user_name = input("Give me your full name in incorrect casings please = ").title()
 
 #print
+print(user_name)
