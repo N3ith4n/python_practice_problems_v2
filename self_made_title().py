607505ff3a595be3dfa2bcda2e95f6eb75e5ab59
Vince <vince.maano1@gmail.com>
2025-03-29 01:00:48 +0800

    added the custom_title and input function

--- self_made_title().py
+++ self_made_title().py
@@ -1,5 +1,12 @@
 #make a function that works the same as title()
+def custom_title(input_string):
+    words = input_string.split()
+    
+    capitalized_words = [word[0].upper() + word[1:].lower() if len(word) > 0 else word for word in words]
+  
+    return ' '.join(capitalized_words)
 
 #asks for user input
+user_input = input("Give me a random sentence = ")
 
 #print
