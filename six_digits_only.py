766fb2bd91203d409d6d8d693732dbc772076828
Vince <vince.maano1@gmail.com>
2025-03-29 02:19:06 +0800

    added all of the functions
    I added all of them at once again cuz its a pretty short code

--- six_digits_only.py
+++ six_digits_only.py
@@ -1,7 +1,10 @@
 #Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
 #asks for user input
+user_number = input("Give me a number from 0-1000 = ")
 
 #the actual program
+fill = user_number.zfill(6)
 
 #print
+print(fill)
