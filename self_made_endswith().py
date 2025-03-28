15280b87b720ef7315b52c1b9d3c3c1657bdcdd8
Vince <vince.maano1@gmail.com>
2025-03-28 23:57:58 +0800

    added the custom_endswith and input function

--- self_made_endswith().py
+++ self_made_endswith().py
@@ -1,5 +1,11 @@
 #make a function just like endswith
+def custom_endswith(input_string, suffix):
+    if len(input_string) < len(suffix):
+        return False
+    return input_string[-len(suffix):] == suffix
 
 #asks for user input
+user_input = input("Give me a random word =  ")
+suffix = input("Enter the suffix to check = ")
 
 #print
