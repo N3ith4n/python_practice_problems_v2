3885ec20dfb8c691458fa5deb47f1d6a9fe29d10
Vince <vince.maano1@gmail.com>
2025-03-29 00:02:54 +0800

    added the custom_ljust and input function

--- self_made_ljust().py
+++ self_made_ljust().py
@@ -1,5 +1,14 @@
 #make a function that works just liike ljust()
+def custom_ljust(input_string, width):
+    if len(input_string) >= width:
+        return input_string
+      
+    spaces_to_add = width - len(input_string)
+    
+    return input_string + ' ' * spaces_to_add
 
 #asks for user input
+user_input = input("Enter a random word = ")
+width = int(input("What is your desired string width? = "))
 
 #print
