0bbf573b6fcd11dcc330a8058ceb234fa7fbd32d
Vince <vince.maano1@gmail.com>
2025-03-29 00:51:48 +0800

    added the custom_swapcase and the input functio

--- self_made_swapcase().py
+++ self_made_swapcase().py
@@ -1,5 +1,18 @@
 #make a function that functions just like swapcase()
+def custom_swapcase(input_string):
+    swapped_string = ""
+
+    for character in input_string:
+        if 'a' <= character <= 'z':
+            swapped_string += character.upper() 
+        elif 'A' <= character <= 'Z':
+            swapped_string += character.lower()
+        else:
+            swapped_string += character
+
+    return swapped_string
 
 #ask for user input
+user_input = input("Enter a random word or sentence = ")
 
 #print
