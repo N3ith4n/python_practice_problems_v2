4be87ffde57d5875bb0316e0252d0b6c782df272
Vince <vince.maano1@gmail.com>
2025-03-29 02:24:11 +0800

    added all of the functions
    I forgot I can just do it like this so I made it this way

--- lower_to_capital_converter.py
+++ lower_to_capital_converter.py
@@ -1,7 +1,7 @@
 #Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 #ask for user input
-
-#the actual program
+user_name = input("What is your full name? = ").lower().upper()
 
 #print
+print(user_name)
