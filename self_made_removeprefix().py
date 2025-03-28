e823f912d5caa241aa1addce4947a2a2988348b0
Vince <vince.maano1@gmail.com>
2025-03-28 23:35:42 +0800

    removed all the comments

--- self_made_removeprefix().py
+++ self_made_removeprefix().py
@@ -1,8 +1,8 @@
 #makes a def where it will check if the input starts with the prefix
 def custom_removeprefix(user_input, prefix):
-    if user_input.startswith(prefix):  #this checks if the the string starts with a prefix
-        return user_input[len(prefix):]  #uses slicing to remove the prefix
-    return user_input  #returns the original string if no prefix is found
+    if user_input.startswith(prefix):
+        return user_input[len(prefix):]
+    return user_input
 
 #asks the user for input
 random_word = input("Enter a random word: ")
@@ -10,4 +10,4 @@ prefix = input("Enter the prefix to remove: ")
 
 #print
 result = custom_removeprefix(random_word, prefix)
-print(f"'{result}'")  # Shows the result after removing the prefix
+print(f"'{result}'")
