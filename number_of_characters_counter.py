6c0c1ce6f001d3fb368966b8b734594d5867475a
Vince <vince.maano1@gmail.com>
2025-03-29 03:02:54 +0800

    added all of the functions
    well I only had to use len so I just the whole program

--- number_of_characters_counter.py
+++ number_of_characters_counter.py
@@ -1,5 +1,8 @@
 #Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 #the actual program
+user_name = input("Give me your full name = ")
+counter = len(user_name)
 
 #print
+print(counter)
