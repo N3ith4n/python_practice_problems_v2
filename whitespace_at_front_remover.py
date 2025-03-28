7c8d7e44297d9af7be073beb175fde42a5f2d38b
Vince <vince.maano1@gmail.com>
2025-03-29 02:24:35 +0800

    made the variable more descriptive

--- whitespace_at_front_remover.py
+++ whitespace_at_front_remover.py
@@ -1,7 +1,7 @@
 #Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 #asks for user input
-name = input("Please give me your name and add some space at front too = ")
+user_name = input("Please give me your name and add some space at front too = ")
 
 #the actual program
 changed = name.lstrip()
