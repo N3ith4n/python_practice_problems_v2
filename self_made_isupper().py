d9621029ee5a5f0fa810f16c564008dc4591fe39
Vince <vince.maano1@gmail.com>
2025-03-28 23:49:58 +0800

    added the custom_is_upper function and input function

--- self_made_isupper().py
+++ self_made_isupper().py
@@ -1,5 +1,11 @@
 #makes a function that is like isupper()
-
+def custom_is_upper(input_string):
+    for character in input_string:
+        if character.isalpha() and not ('A' <= character <= 'Z'):
+            return False
+    return True
+  
 #take user input
+user_input = input("Input your name お願いします: ")
 
 #print
