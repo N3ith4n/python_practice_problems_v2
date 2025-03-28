18f536e0fa08bdb18f0e8815ee2849043115f10b
Vince <vince.maano1@gmail.com>
2025-03-29 02:48:43 +0800

    Create number_of_words_counter.py and added the pseudocode

---
+++ number_of_words_counter.py
@@ -0,0 +1,5 @@
+#Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+#the actual program
+
+#print the total number of words in the string
