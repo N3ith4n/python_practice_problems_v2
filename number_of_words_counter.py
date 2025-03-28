a5ea9927b8b528b4a4695c5d89e7d6651e441c86
Vince <vince.maano1@gmail.com>
2025-03-29 02:57:32 +0800

    added all of the functions

--- number_of_words_counter.py
+++ number_of_words_counter.py
@@ -1,5 +1,8 @@
 #Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 
 #the actual program
+user_statement = input("Give me a statement to count the number of words in = ").split()
+seperated_words = len(user_statement)
 
 #print the total number of words in the string
+print(seperated_words)
