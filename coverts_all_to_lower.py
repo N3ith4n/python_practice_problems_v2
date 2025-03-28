b621b7e2b9d89796065459e4e56e3f14b12670ef
Vince <vince.maano1@gmail.com>
2025-03-29 02:27:15 +0800

    added all of the functions

--- coverts_all_to_lower.py
+++ coverts_all_to_lower.py
@@ -1,5 +1,7 @@
 #Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
 
 #the actual program
+user_name = input("What is your full name? = ").lower()
 
 #print
+print(user_name)
