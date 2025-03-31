413b771779bb1ae503d84aca19f1da7a25070844
Vince <vince.maano1@gmail.com>
2025-04-01 01:06:19 +0800

    added the main function and input function

--- self_made_index().py
+++ self_made_index().py
@@ -1,6 +1,13 @@
 #make a function that works just like index()
+def custom_index(input_string, substring):
+    for i in range(len(input_string) - len(substring) + 1):
+        if input_string[i:i + len(substring)] == substring:
+            return i
+    raise ValueError(f"'{substring}' not found in string")
 
 #asks for user input
+user_string = input("Enter a string = ")
+substring = input("Enter the substring to find = ")
 
 #print
 
