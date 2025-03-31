518ff03bd8434772d2a7eb2d97f168027c7ad1a8
Vince <vince.maano1@gmail.com>
2025-03-31 20:36:05 +0800

    added all of the functions
    another program that just needs small tweaking so I added all of it at once

--- self_made_rjust().py
+++ self_made_rjust().py
@@ -1,5 +1,16 @@
 #make a function that works just liike rjust()
+def custom_rjust(input_string, width):
+    if len(input_string) >= width:
+        return input_string
+      
+    spaces_to_add = width - len(input_string)
+    
+    return ' ' * spaces_to_add + input_string
 
 #asks for user input
+user_input = input("Enter a random word = ")
+width = int(input("What is your desired string width? = "))
 
 #print
+result = custom_rjust(user_input, width)
+print(f"Here is the adjusted string with your desired string width = '{result}'")
