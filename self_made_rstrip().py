2da45c2270e45802f3723f50b723b3e545e27987
Vince <vince.maano1@gmail.com>
2025-03-31 17:07:41 +0800

    added the input and print function
    if you have noticed I have included ' ' in the print message now just because you cant really see the difference unless we have a (') as an indicator of the end of the string

--- self_made_rstrip().py
+++ self_made_rstrip().py
@@ -8,5 +8,8 @@ def custom_rstrip(input_string):
     return input_string[:end_index + 1]
 
 #asks the user to input their name + some spaces after it
+user_name = input("Please enter your full name and add some spaces at the end = ")
 
 #print
+result = custom_rstrip(user_name)
+print(f"I removed the spaces at the end for you = '{result}'")
