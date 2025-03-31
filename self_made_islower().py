8eac38313b0a767d1b7010fb62f4d63830eec5ee
Vince <vince.maano1@gmail.com>
2025-03-31 20:16:21 +0800

    added all of the functions
    once again added it all in one go as its just a minor tweak from the isupper program

--- self_made_islower().py
+++ self_made_islower().py
@@ -1,5 +1,13 @@
 #makes a function that is like islower()
+def custom_is_lower(input_string):
+    for character in input_string:
+        if character.isalpha() and not ('a' <= character <= 'z'):
+            return False
+    return True
   
 #take user input
+user_input = input("Input your name お願いします = ")
 
 #print
+result = custom_is_lower(user_input)
+print(f"Is your name all in lowercase? {result}")
