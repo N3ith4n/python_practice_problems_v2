3305cc680843ce864fc18938b5451402810d8f70
Vince <vince.maano1@gmail.com>
2025-03-31 21:15:02 +0800

    added all of the functions
    pretty interesting. I decided to add the code all at once as Its actually just the same as rjust

--- self_made_zfill().py
+++ self_made_zfill().py
@@ -1,5 +1,16 @@
 #make a function that works just like zfill()
+def custom_zfill(input_string, width):
+    if len(input_string) >= width:
+        return input_string
+
+    zeros_to_add = width - len(input_string)
+
+    return '0' * zeros_to_add + input_string
 
 #asks for user input
+user_input = input("Enter a number = ")
+width = int(input("What is your desired string width? = "))
 
 #print
+result = custom_zfill(user_input, width)
+print(f"Here is your number with added zeroes up front to match the width you want = {result}")
