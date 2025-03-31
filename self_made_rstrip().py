2f8ecd5e420ee49516ad6dd17f75ebeae234bcce
Vince <vince.maano1@gmail.com>
2025-03-31 17:05:09 +0800

    Create self_made_rstrip().py and added the pseudocode

---
+++ self_made_rstrip().py
@@ -0,0 +1,5 @@
+#make a function that will start reading the string from right to left and find the last character, thus removing the spaces at the end
+
+#asks the user to input their name + some spaces after it
+
+#print
