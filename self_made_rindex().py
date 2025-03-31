e0f27e154b0ba1a2fb5bd5658fa4294958f1fdfe
Vince <vince.maano1@gmail.com>
2025-04-01 01:20:23 +0800

    added all of the functions
    I don't really get why this is necessary as it pretty much functions the same as index. Well anyways, I decided to add it all at once as I just needed to change the for loop to negative

--- self_made_rindex().py
+++ self_made_rindex().py
@@ -1,5 +1,17 @@
 #make a function that works just like rindex()
+def custom_rindex(input_string, substring):
+    for i in range(len(input_string) - len(substring), -1, -1):
+        if input_string[i:i + len(substring)] == substring:
+            return i
+    raise ValueError(f"'{substring}' not found in string")
 
 #asks for user input
+user_string = input("Enter a string = ")
+substring = input("Enter the substring to find = ")
 
 #print
+try:
+    result = custom_rindex(user_string, substring)
+    print(f"The substring '{substring}' starts at index {result}.")
+except ValueError as error:
+    print(error)
