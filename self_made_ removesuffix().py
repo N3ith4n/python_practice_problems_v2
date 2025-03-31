5fd3fb9cd6848a627906ba0cde387bd3b1d78f79
Vince <vince.maano1@gmail.com>
2025-03-31 17:24:49 +0800

    added the main function
    that was interesting and much easier than I thought

--- self_made_ removesuffix().py
+++ self_made_ removesuffix().py
@@ -1,4 +1,8 @@
 #makes a def where it will check if the input ends with the suffix
+def custom_removesuffix(user_input, suffix):
+    if user_input.endswith(suffix):
+        return user_input[:len(user_input) - len(suffix)]
+    return user_input
 
 #asks the user for input
 
