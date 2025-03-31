4b1c4c963aade64083a368a0f1120f4df4aa5155
Vince <vince.maano1@gmail.com>
2025-03-31 20:25:31 +0800

    added all of the functions
    once again added it all at once as it was just a minor tweak

--- self_made_startswith().py
+++ self_made_startswith().py
@@ -1,5 +1,13 @@
 #make a function just like startswith
+def custom_startswith(input_string, prefix):
+    if len(input_string) < len(prefix):
+        return False
+    return input_string[:len(prefix)] == prefix
 
 #asks for user input
+user_input = input("Give me a random word =  ")
+prefix = input("Enter the prefix to check = ")
 
 #print
+result = custom_startswith(user_input, prefix)
+print(f"Does the word you gave starts with'{prefix}'? {result}!")
