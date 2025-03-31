2296f3f4e609642f0754ab826ed165705483d7eb
Vince <vince.maano1@gmail.com>
2025-03-31 17:06:12 +0800

    added just the main function
    kinda struggled reversing the function of my lstrip so I'm only uploading this, I think this is worthy of a commit

--- self_made_rstrip().py
+++ self_made_rstrip().py
@@ -1,4 +1,11 @@
 #make a function that will start reading the string from right to left and find the last character, thus removing the spaces at the end
+def custom_rstrip(input_string):
+    end_index = len(input_string) - 1
+
+    while end_index >= 0 and input_string[end_index] == " ": 
+        end_index -= 1
+    
+    return input_string[:end_index + 1]
 
 #asks the user to input their name + some spaces after it
 
