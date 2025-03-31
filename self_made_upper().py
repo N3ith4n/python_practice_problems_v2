5830fc532be03e1ed4c4380e08dd4f300da8884a
Vince <vince.maano1@gmail.com>
2025-03-31 18:21:30 +0800

    added all of the functions
    I only made a slight change from the code of lower() so I just added the whole code in one go

--- self_made_upper().py
+++ self_made_upper().py
@@ -1,5 +1,19 @@
 #makes a function like the upper() function
+def custom_upper(input_string):
+    uppercase_string = ""
+    
+    for character in input_string:
+        if 'a' <= character <= 'z':
+            uppercase_character = chr(ord(character) - 32)
+            uppercase_string += uppercase_character
+        else:
+            uppercase_string += character
+            
+    return uppercase_string
 
 #get user input
+user_input = input("Give me your name: ")
 
 #prints
+result = custom_upper(user_input)
+print(f"Here is the upper case version of your name: {result}")
