b133722ce51e4e595fa5f4d70e6ae5af53ff1c9e
Vince <vince.maano1@gmail.com>
2025-03-31 23:49:02 +0800

    added the input and print function

--- self_made_count().py
+++ self_made_count().py
@@ -10,7 +10,10 @@ def custom_count(input_string, substring):
 
     return count
 
-
 #asks for user input
+user_string = input("Enter a string = ")
+counter = input("Enter the substring to count = ")
 
 #print
+result = custom_count(user_string, counter)
+print(f"The substring '{counter}' appears {result} times.")
