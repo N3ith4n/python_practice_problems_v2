52dcc8ae97c78758272f3befcdddedffb2f5aad6
Vince <vince.maano1@gmail.com>
2025-03-31 23:47:12 +0800

    added the main function

--- self_made_count().py
+++ self_made_count().py
@@ -1,4 +1,15 @@
 #make a function that works just like count()
+def custom_count(input_string, substring):
+    count = 0
+    index = 0
+    
+    while index <= len(input_string) - len(substring):
+        if input_string[index:index + len(substring)] == substring:
+            count += 1
+        index += 1
+
+    return count
+
 
 #asks for user input
 
